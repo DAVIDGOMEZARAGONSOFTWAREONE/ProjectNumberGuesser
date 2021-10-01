ac92f94bb0c0a2e0e40e3ba67818f920fc3122be
DAVIDGOMEZARAGONSOFTWAREONE <david.garagon@softwareone.com>
2021-10-02 00:14:37 +0200

    inital commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+function generateTarget(){
+    let secretNumber=Math.floor(Math.random() * 9) + 0;
+    return secretNumber;
+}
+
+function compareGuesses(currentHumanGuess,computerGuess,target){
+
+    let victoria;
+
+    humanResult=Math.abs(currentHumanGuess-target);
+    computerResult=Math.abs(computerGuess-target);    
+
+    if (humanResult==0)victoria=true;
+    else if(humanResult<=computerResult)victoria=true;
+    else victoria=false;
+    
+    return victoria;
+}
+
+function updateScore(winner){
+
+    if (winner=="human")humanScore++;
+    else computerScore++;
+
+}
+
+function advanceRound(){
+    currentRoundNumber++;
+}
+
+
